a696fd9445883f7edef4c77c7839a56e7a848ddb
silviotron <silvionovasmartinezgmail.com>
2022-03-16 14:28:46 +0100

    upgrade

--- scripts/mode.js
+++ scripts/mode.js
@@ -8,19 +8,18 @@ function oscuro_set() {
   r.style.setProperty("--text-color", "#ffffff");
   r.style.setProperty("--bg-color", "#2f0300e8");
   r.style.setProperty("--cookie-filter", "invert(1)");
-  r.style.setProperty("--moon-color", "#ffffff");
-  r.style.setProperty("--sun-color", "transparent");
+  document.cookie = "modo = claro";
+
 }
 function claro_set() {
   // Set the value of variable --blue to another value (in this case "lightblue")
   r.style.setProperty("--main-color", "#ff0000b7");
-  r.style.setProperty("--second-color", "#ffffff");
+  r.style.setProperty("--second-color", "#f0f0f0");
   r.style.setProperty("--text-color", "#000000");
-  r.style.setProperty("--bg-color", "#f0f0f0");
+  r.style.setProperty("--bg-color", "#ffffff");
   r.style.setProperty("--cookie-filter", "invert(0)");
+  document.cookie = "modo = oscuro;"
 
-  r.style.setProperty("--moon-color", "transparent");
-  r.style.setProperty("--sun-color", "#000000");
 }
 function modo_set() {
   var checkBox = document.getElementById("myCheck");
